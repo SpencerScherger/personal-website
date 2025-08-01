a7681c39cf55aff56885e7c1ad132a716e55ef10
SpencerScherger <spencerscherger@gmail.com>
2025-07-31 20:36:25 -0500

    Fix Quotes for ESLint
    2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,11 +3,11 @@ export default function About() {
       <div>
         <h1 className="text-2xl font-semibold mb-3">About Me</h1>
         <p>
-          I&apos;'m a rising junior at Texas Christian University pursuing a dual degree in Mathematics and
+          I&apos;m a rising junior at Texas Christian University pursuing a dual degree in Mathematics and
           Computer Science. I enjoy theoretical computer science, pure math, and quantitative finance.
         </p>
         <p className="mt-2">
-          I&apos;'m especially interested in topics like number theory, group theory, machine learning, and
+          I&apos;m especially interested in topics like number theory, group theory, machine learning, and
           their applications in research and industry.
         </p>
       </div>
